b43b9053f5f99c261e753cc0438f7acd5e30b009
MFA <mehmetfatihates.cs@gmail.com>
2022-12-26 17:58:04 +0300

    26/12/2022

---
+++ 4. Ruby/Ruby Essential Training Part 1- The Basics/CH2-Ruby Object Types/Roman Numerals.rb
@@ -0,0 +1,13 @@
+# Convert number to Roman numerals
+number_map = {
+    1 =>'I',
+    2 =>'II',
+    3 =>'III',
+    4=>'IV',
+    5 =>'V',
+    6=>'VI',
+    7=>'VII',
+    8=>'VIII',
+    9=>'IX',
+    10=>'X'}
+puts number_map(2)
